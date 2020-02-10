b15aec926f94cda9c0dae79eb716c33570b2c8ec
Meghna Tanwal <tanwalmeghna@gmail.com>
2020-02-11 02:24:15 +0530

    Add Library and Book class

---
+++ src/main/java/com/twu/biblioteca/Library.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> books;
+
+    public Library(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,24 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+
+class LibraryTest {
+
+    @Test
+    public void shouldGetABook() {
+        Book book = mock(Book.class);
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(book);
+        Library library = new Library(books);
+
+        assertThat(books, is(equalTo(library.getBooks())));
+    }
+
+}

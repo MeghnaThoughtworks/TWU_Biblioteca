6a31af40bff0e2987749a425c6601a0940965f2d
Meghna Tanwal <tanwalmeghna@gmail.com>
2020-02-12 14:05:29 +0530

    Refactor library test cases

--- src/test/java/com/twu/biblioteca/LibraryTest.java
+++ src/test/java/com/twu/biblioteca/LibraryTest.java
@@ -12,35 +12,26 @@ import static org.mockito.Mockito.mock;
 
 class LibraryTest {
     Library library;
+    Book book;
     Book book1;
-    ArrayList<Book> books = new ArrayList<>();
 
     @BeforeEach
-    public void init(){
-        book1 = new Book("1","1",1);
-        books.add(book1);
-    }
-
-    @Test
-    public void shouldGetABook() {
-        Book book = mock(Book.class);
+    public void init() {
+        book = mock(Book.class);
+        book1 = mock(Book.class);
         ArrayList<Book> books = new ArrayList<>();
         books.add(book);
-        Library library = new Library(books);
-
-        assertThat(books, is(equalTo(library.getBooks())));
-    }
-    @Test
-    public void shouldCheckoutABook(){
-        Book book2 = new Book("2","2",2);
-        books.add(book2);
-        ArrayList<Book> availableBooks = new ArrayList<>();
-        availableBooks.add(book2);
+        books.add(book1);
         library = new Library(books);
-
-        library.checkout(book1.getTitle());
-
-        assertThat(availableBooks,is(equalTo(library.getBooks())));
     }
+
+    @Test
+    public void shouldCheckoutABook() {
+        library.checkout(book);
+        String expectedResult = "(1) "+book1.toString()+"\n";
+
+        assertThat(expectedResult,is(equalTo(library.toString())));
+    }
+
 
 }

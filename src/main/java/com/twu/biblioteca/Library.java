899be59cc971ac8c2cfd1b58650fbf2a621e8ab3
Meghna Tanwal <tanwalmeghna@gmail.com>
2020-02-12 12:51:44 +0530

    Changed the Library classs

--- src/main/java/com/twu/biblioteca/Library.java
+++ src/main/java/com/twu/biblioteca/Library.java
@@ -5,24 +5,40 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> books;
+    private ArrayList<Book> checkedOutBooks = new ArrayList<>();
 
     public Library(ArrayList<Book> books) {
         this.books = books;
     }
 
-    public ArrayList<Book> getBooks() {
-        return books;
+    public void checkout(Book bookEntered) {
+        Book checkedOutBook = null;
+        for (Book book : books) {
+            if (book.equals(bookEntered))
+                checkedOutBook = book;
+        }
+        if (checkedOutBook != null) {
+            books.remove(checkedOutBook);
+            checkedOutBooks.add(checkedOutBook);
+        }
     }
 
-    public void checkout(String title) {
-        Book bookCheckedOut = null;
-        for (Book book : this.getBooks()) {
-            if (book.getTitle().toLowerCase().equals(title))
-                bookCheckedOut = book;
+    public boolean checkedBookOut(Book bookEntered) {
+        for (Book book : checkedOutBooks) {
+            if (book.equals(bookEntered))
+                return true;
         }
-        if (bookCheckedOut != null) {
-            this.getBooks().remove(bookCheckedOut);
-//            this.checkedOutBooks.add(bookCheckedOut);
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = 0;
+        for (Book book : books) {
+            stringBuilder.append("(").append(i + 1).append(") ").append(book).append("\n");
+            i++;
         }
+        return stringBuilder.toString();
     }
 }
